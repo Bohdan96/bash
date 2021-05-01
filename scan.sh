438028d3158be06a795d8e1cf42809d45cc4a483
Bohdan96 <64528224+Bohdan96@users.noreply.github.com>
2021-05-02 02:07:11 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,32 +1,43 @@
+
 #!/bin/bash
-menu () {
-        echo "scan by Kanarskyi Bohdan"
+menu() {
+        echo "-=-=-=-=-=-=-=-=-=-=-=-scan by Kanarskyi Bohdan-=-=-=-=-=-=-=-=-=-=-=-"
         echo "Usage: scan [OPTIONS]"
         echo "OPTIONS:"
-        echo "--all, -a:"
-        echo "     Displays the IP addresses of all hosts in the current subnet"
-        echo "--target, -t [IP] [PORT]:"
-        echo "     Displays a list of open system TCP ports"
+        echo "   --all, -a:"
+        echo "         Displays the IP addresses of all hosts in the current subnet"
+        echo "   --target, -t [IP] [PORT]:"
+        echo "         Displays a list of open system TCP ports"
 }
-port_scan () {
-        if [ "$1" == '' ]
-        then
-        echo "Wait while ports are scanned..."
-        `nc -nvz $1 22` #> $2.txt 2>&1
-        else
-        echo "Error: Incorrect command syntax"
-        echo "Use: ./scan.sh -t [IP] [PORT]"
-        fi
-        #tac $2.txt
-        #rm -rf $2.txt
+
+##################function fot scan port##################
+scan_port() {
+        echo "-------------------Scanning port---------------------"
+        echo
+        ip=$2
+        port=$3
+        nc -nvz $ip $port
+        echo
+        echo "-----------------------DONE--------------------------"
 }
+##########################################################
+
+##################function for show ip####################
+show_ip() {
+        echo "-----------------------------------------------------"
+        echo "             This server has next ip:          "
+        ip -4 a | grep inet | cut -f1 | awk '{print $2}' | cut -f1,2,3
+        echo "-----------------------------------------------------"
+}
+##########################################################
+
 if [ -n "$1" ]
 then
 case "$1" in
--a) echo `ip a` ;;
---all) echo `ip a` ;;
--t) port_scan ;;
---target) port_scan ;;
+-a) show_ip $* ;;
+--all) show_ip $* ;;
+-t) scan_port $* ;;
+--target) scan_port $* ;;
 *) menu ;;
 esac
 shift
